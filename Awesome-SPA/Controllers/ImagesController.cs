0d78a938d4e2b4726ca82a976d3f2499a943ea22
magnushg <magnus.h.green@gmail.com>
2012-10-21 13:38:22 +0200

    Renamed imagesController

---
+++ Awesome-SPA/Controllers/ImagesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Awesome_SPA.Services;
+using Awsome_SPA.Models;
+
+namespace Awesome_SPA.Controllers
+{
+    public class ImagesController : ApiController
+    {
+        private readonly IInstagramService _instagramService;
+
+        public ImagesController(IInstagramService instagramService)
+        {
+            _instagramService = instagramService;
+        }
+
+        // GET api/values
+        public InstagramData Get()
+        {
+            return _instagramService.GetImagesFromTag("bouvet");
+        }
+
+        // GET api/values/5
+        public InstagramData Get(string searchTerm)
+        {
+            return _instagramService.GetImagesFromTag(searchTerm);
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
