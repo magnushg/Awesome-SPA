20ab1f974061e13412d9e79b69cb7093ddc8750d
magnushg <magnus.h.green@gmail.com>
2012-10-24 00:58:54 +0200

    Added simple schedule task

--- Awesome-SPA/Services/IInstagramService.cs
+++ Awesome-SPA/Services/IInstagramService.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Timers;
 using Awesome_SPA.Models;
 using Newtonsoft.Json;
 
@@ -14,6 +16,13 @@ namespace Awesome_SPA.Services
 
     public class InstagramService : IInstagramService
     {
+        private readonly INotifier _notifier;
+
+        public InstagramService(INotifier notifier)
+        {
+            _notifier = notifier;
+        }
+
         public IEnumerable<InstagramBasicData> GetImagesFromTag(string searchTerm)
         {
             var address = string.Format("https://api.instagram.com/v1/tags/{0}/media/recent?access_token=24613827.f59def8.557cc0f5848b4738b417ef677d2ced5a", searchTerm);
@@ -29,8 +38,57 @@ namespace Awesome_SPA.Services
                                                                           image_standard_res = d.images.standard_resolution.url,
                                                                           likes = d.likes.count
                                                                       });
+
+            var serializedData = JsonConvert.SerializeObject(instagramData);
+            var schedule = new ScheduleJob(() => _notifier.Notify(serializedData));
+            schedule.Start();
             return instagramData;
         }
+    }
+
+    public class ScheduleJob
+    {
+        private readonly Timer _timer;
+        private Action _job;
+
+        public ScheduleJob(Action task)
+        {
+            _timer = new Timer();
+            _job = task;
+        }
+
+        public void Start()
+        {
+            try
+            {
+                _timer.Interval = (double)TimeSpan.FromSeconds(30).TotalMilliseconds;
+
+                _timer.Elapsed += _timer_Elapsed;
+                _timer.Start();
+                IsRunning = true;
+            }
+            catch (Exception e)
+            {
+                IsRunning = false;
+                throw e;
+            }
+        }
+        public void Stop()
+        {
+            if (_timer != null)
+                _timer.Stop();
+
+            IsRunning = false;
+        }
+        private void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            _timer.Enabled = false;
+
+            _job(); //perform the job
+
+            _timer.Enabled = true;
+        }
+        public bool IsRunning { get; set; }
     }
 
     public class InstagramBasicData
