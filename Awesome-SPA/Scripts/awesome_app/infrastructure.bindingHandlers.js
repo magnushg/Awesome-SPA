178f05602d889d0eb2ac14b8f32c7fbd12e41f87
magnushg <magnus.h.green@gmail.com>
2012-10-25 14:49:03 +0200

    Added likes to popover

--- Awesome-SPA/Scripts/awesome_app/infrastructure.bindingHandlers.js
+++ Awesome-SPA/Scripts/awesome_app/infrastructure.bindingHandlers.js
@@ -4,7 +4,8 @@
             var value = ko.utils.unwrapObservable(valueAccessor());
             var username = value.user || '';
             var caption = value.caption || '';
-            $(element).popover({ trigger: 'hover', placement: 'bottom', html: true, title: '<b>' + username + '<b>', content: caption });
+            var likes = value.likes || '';
+            $(element).popover({ trigger: 'hover', placement: 'bottom', html: true, title: '<b>' + username + '<b>', content: '<p>'+ caption + '</p><p><b>Likes : ' + likes + '</b></p>'  });
         },
         update: function (element, valueAccessor, allBindingsAccessor) {
             // Leave as before
